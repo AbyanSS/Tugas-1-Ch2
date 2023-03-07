3cb2c66f39ae0ec95246da917dd09d91926c2a91
AbyanSS <abyansurya04@gmail.com>
2023-03-07 23:32:18 +0700

    FizzBuzz - Arj

--- Tugas1-Ch2/src/Main.java
+++ Tugas1-Ch2/src/Main.java
@@ -3,13 +3,8 @@ public class Main {
     private int endNumber;
 
     public static void main(String[] args) {
-        printFizzBuzz fb = new printFizzBuzz(1,50);
-        fb.pFizzBuzz();
-
-        printFizz fizz = new printFizz(1, 50);
-        fizz.pFizz();
-
-        printBuzz buzz = new printBuzz(1,50);
-        buzz.pBuzz();
+        printFizzBuzz fb = new printFizzBuzz(1,50); fb.pFizzBuzz();
+        printFizz fizz = new printFizz(1, 50); fizz.pFizz();
+        printBuzz buzz = new printBuzz(1,50); buzz.pBuzz();
     }
 }
